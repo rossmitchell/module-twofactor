c8b4cc4d30dfb244e35b17261968ae8907f2b32a
Ross Mitchell <ross@edmondscommerce.co.uk>
2017-04-16 20:45:38 +0100

    Adding code style rules

---
+++ src/Setup/InstallSchema.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * A two factor authentication module that protects both the admin and customer logins
+ * Copyright (C) 2017  Ross Mitchell
+ *
+ * This file is part of Rossmitchell/Twofactor.
+ *
+ * Rossmitchell/Twofactor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Rossmitchell\Twofactor\Setup;
+
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface   $setup
+     * @param ModuleContextInterface $context
+     *
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+        $tableAdmins = $installer->getTable('admin_user');
+
+        $installer->getConnection()->addColumn(
+            $tableAdmins,
+            'use_two_factor',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                'nullable' => true,
+                'default' => 0,
+                'comment' => 'Use Two Factor Authentication',
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $tableAdmins,
+            'two_factor_secret',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                'nullable' => true,
+                'comment' => 'Two Factor Secret',
+            ]
+        );
+
+        $installer->endSetup();
+    }
+}
