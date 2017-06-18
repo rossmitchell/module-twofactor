a6a2553dcb840c7bb40447860f6ac52892494b51
Ross Mitchell <ross.mitchell@gmail.com>
2017-06-18 22:48:23 +0100

    well that didn't work - let's try something else

--- .travis/run-scripts.bash
+++ .travis/run-scripts.bash
@@ -4,7 +4,7 @@ cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/${TEST_TYPE}
 
 if [[ ${GENERATE_COVERAGE} == "1" ]]
 then
-    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-clover=${TRAVIS_BUILD_DIR}/coverage.xml
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-clover=${TRAVIS_BUILD_DIR}/build/logs/clover.xml
 else
     ../../../vendor/bin/phpunit -c $PWD/phpunit.xml
 fi
