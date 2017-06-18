9f9c2f103ef4d8a78719facc50070170980a0c5d
Ross Mitchell <ross.mitchell@gmail.com>
2017-06-19 00:09:03 +0100

    Have run into the same problem using this - going to try and merge the files

--- .travis/run-scripts.bash
+++ .travis/run-scripts.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/${TEST_TYPE}
+
 
 if [[ ${GENERATE_COVERAGE} == "1" ]]
 then
-    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-clover=${TRAVIS_BUILD_DIR}/build/logs/clover.xml
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/integration
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-integration.cov
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/unit
+    ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-unit.cov
+    php ${TRAVIS_BUILD_DIR}/vendor/bin/phpcov.php merge --clover ${TRAVIS_BUILD_DIR}/build/logs/clover.xml ${TRAVIS_BUILD_DIR}/build/cov
 else
+    cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/${TEST_TYPE}
     ../../../vendor/bin/phpunit -c $PWD/phpunit.xml
 fi
