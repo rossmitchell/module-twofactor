b43eef073b3dac63d4321c60a24440d65828fc17
Ross Mitchell <ross.mitchell@gmail.com>
2017-06-19 01:10:55 +0100

    I will get this working

--- .travis/run-scripts.bash
+++ .travis/run-scripts.bash
@@ -8,7 +8,7 @@ then
     ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-integration.cov
     cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/unit
     ../../../vendor/bin/phpunit -c $PWD/phpunit.xml --coverage-text --coverage-php=${TRAVIS_BUILD_DIR}/build/cov/clover-unit.cov
-    php ${TRAVIS_BUILD_DIR}/vendor/bin/phpcov.php merge --clover ${TRAVIS_BUILD_DIR}/build/logs/clover.xml ${TRAVIS_BUILD_DIR}/build/cov
+    php ${TRAVIS_BUILD_DIR}/magento2/vendor/bin/phpcov.php merge --clover ${TRAVIS_BUILD_DIR}/build/logs/clover.xml ${TRAVIS_BUILD_DIR}/build/cov
 else
     cd ${TRAVIS_BUILD_DIR}/magento2/dev/tests/${TEST_TYPE}
     ../../../vendor/bin/phpunit -c $PWD/phpunit.xml
